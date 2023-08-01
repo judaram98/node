d5520cc1e3673d9a6b1dabe5ced470174187e09e
Juan David <judaram98@gmail.com>
2023-08-01 10:55:11 -0600

    Monohilo

---
+++ conceptos/monohilo.js
@@ -0,0 +1,14 @@
+console.log("hola");
+
+let i = 0;
+setInterval(function () {
+  console.log(i);
+  i++;
+
+  if (i === 5) {
+    console.log("Forzamos error");
+    var a = 3 + z;
+  }
+}, 1000);
+
+console.log("Segunda instrucción");
