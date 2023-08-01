e73d9eb38c667ca0ba5e343987c790e5fbc84f05
Juan David <judaram98@gmail.com>
2023-08-01 12:52:53 -0600

    modulos

---
+++ modulos/globales.js
@@ -0,0 +1,14 @@
+// let i = 0;
+// let intervalo = setInterval(function () {
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+//   console.log("Hola");
+// }, 1000);
+
+// setImmediate(function () {
+//   console.log("hola");
+// });
+
+globalThis.miVariable = "Hola mundo"; //Esta es una variable global
