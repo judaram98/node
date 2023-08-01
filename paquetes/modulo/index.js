9e12ac8e4abe40d0fabc48bfcaf90cd3105424bb
Juan David <judaram98@gmail.com>
2023-08-01 16:37:11 -0600

    modulos propios

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,7 @@
+//Traer nuestro modulo y ejecutar una funcion del modulo
+const modulo = require("./modulo");
+
+//Ejecutar la funcion del modulo
+console.log(modulo.prop1);
+// modulo();
+modulo.saludar();
