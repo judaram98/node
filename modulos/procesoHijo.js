1ba91139840beb7884037c4c88eaeb9d573d6381
Juan David <judaram98@gmail.com>
2023-08-01 14:35:02 -0600

    child process

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,18 @@
+const { exec, spawn } = require("child_process");
+
+// exec("ls -la", (err, stdout, sterr) => {
+//   if (err) {
+//     console.error(err);
+//     return false;
+//   }
+//   console.log(stdout);
+// });
+
+let proceso = spawn("ls", ["-la"]);
+console.log(proceso.connected);
+proceso.stdout.on("data", function (dato) {
+  console.log(dato.toString());
+});
+proceso.on("exit", function () {
+  console.log("El proceso terminó");
+});
